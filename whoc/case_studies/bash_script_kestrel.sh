dbc8f8fa27297b77586f20bf49d84f0497e41731
Aoife Henry <ahenry@kl3.head.cm.kestrel.hpc.nrel.gov>
2025-04-06 11:23:58 -0600

    res

--- whoc/case_studies/bash_script_kestrel.sh
+++ whoc/case_studies/bash_script_kestrel.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name=full_floris_case_studies.py
-#SBATCH --time=36:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem=0
-#SBATCH --nodes=4
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=104
 #SBATCH --account=ssc
 
 module purge
 module load mamba
-mamba activate whoc
+mamba activate wind_forecasting
 module load intel
 export LD_LIBRARY_PATH=/projects/ssc/ahenry/conda/envs/whoc/lib
 echo $SLURM_NTASKS
 #mpirun -np $SLURM_NTASKS python run_case_studies.py 0 9 -rs -st 3600 -ns 6 -p -m mpi -sd /projects/ssc/ahenry/whoc/floris_case_studies
-mpirun -np $SLURM_NTASKS python run_case_studies.py 0 1 2 3 4 5 6 -rs -st 3600 -ns 6 -p -m mpi -sd /projects/ssc/ahenry/whoc/floris_case_studies
+python run_case_studies.py 18 --wf_source scada --generate_lut -m cf -sd /projects/ssc/ahenry/whoc/floris_case_studies -wcnf $HOME/toolboxes/wind_forecasting_env/wind-hybrid-open-controller/examples/hercules_input_001.yaml -dcnf $HOME/toolboxes/wind_forecasting_env/wind-forecasting/examples/inputs/preprocessing_inputs_kestrel_awaken_new.yaml -mcnf $HOME/toolboxes/wind_forecasting_env/wind-forecasting/examples/inputs/training_inputs_kestrel_awaken.yaml
 #mpirun -np $SLURM_NTASKS python run_case_studies.py 5 6 -p -rs -st 3600 -ns 6 -m mpi -sd /projects/ssc/ahenry/whoc/floris_case_studies
