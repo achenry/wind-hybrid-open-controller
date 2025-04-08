69cb5bbe82d6b1be6e1f5a5f05e2670f8073c64b
Aoife Henry <aoife.henry@colorado.edu>
2025-04-08 08:59:20 -0600

    upd

--- whoc/case_studies/bash_script_kestrel.sh
+++ whoc/case_studies/bash_script_kestrel.sh
@@ -10,8 +10,12 @@ module purge
 module load mamba
 mamba activate whoc
 module load intel
-export LD_LIBRARY_PATH=/projects/ssc/ahenry/conda/envs/whoc/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/projects/ssc/ahenry/conda/envs/whoc/lib
 echo $SLURM_NTASKS
 #mpirun -np $SLURM_NTASKS python run_case_studies.py 0 9 -rs -st 3600 -ns 6 -p -m mpi -sd /projects/ssc/ahenry/whoc/floris_case_studies
 mpirun -np $SLURM_NTASKS python run_case_studies.py 0 1 2 3 4 5 6 -rs -st 3600 -ns 6 -p -m mpi -sd /projects/ssc/ahenry/whoc/floris_case_studies
-#mpirun -np $SLURM_NTASKS python run_case_studies.py 5 6 -p -rs -st 3600 -ns 6 -m mpi -sd /projects/ssc/ahenry/whoc/floris_case_studies
+
+mpirun -np $SLURM_NTASKS python run_case_studies.py 0 1 2 3 4 5 6 -rs -rrs -st 3600 -ns 3 -m mpi \
+       -sd /projects/ssc/ahenry/whoc/floris_case_studies \
+       -wf scada
+    #    -mcnf /home/ahenry/toolboxes/wind_forecasting_env/wind-forecasting/examples/inputs/training_inputs_kestrel_awaken.yaml \
