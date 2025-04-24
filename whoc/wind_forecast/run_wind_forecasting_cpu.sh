2d59364139ab1e6162469252c663c1cd0bfeca4f
Aoife Henry <aoife.henry@colorado.edu>
2025-04-24 16:35:24 -0600

    updated wind forecasting args

--- whoc/wind_forecast/run_wind_forecasting_cpu.sh
+++ whoc/wind_forecast/run_wind_forecasting_cpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=baseline_wf
+#SBATCH --job-name=baseline_wf_cpu
 #SBATCH --account=ssc
-#SBATCH --output=baseline_wf_%j.out
+#SBATCH --output=%j_%x.out
 #SBATCH --nodes=1
 #SBATCH --time=01:00:00
 #SBATCH --partition=debug
@@ -16,14 +16,13 @@ echo "SLURM_JOB_GPUS=${SLURM_JOB_GPUS}"
 echo "SLURM_JOB_GRES=${SLURM_JOB_GRES}"
 echo "SLURM_NTASKS=${SLURM_NTASKS}"
 echo "SLURM_NTASKS_PER_NODE=${SLURM_NTASKS_PER_NODE}"
-echo "NTUNERS=${NTUNERS}"
-echo "NTASKS_PER_TUNER=${NTASKS_PER_TUNER}"
+echo "SLURM_NTASKS_PER_NODE=${SLURM_NTASKS_PER_NODE}"
 
 echo "=== ENVIRONMENT ==="
 module list
 
 export MODELS="kf persistence sf"
-export MODEL_CONFIG_PATH="$HOME/toolboxes/wind_forecasting_env/wind-forecasting/config/training/training_inputs_kestrel_awaken_pred60.yaml"
+export MODEL_CONFIG_PATH="$HOME/toolboxes/wind_forecasting_env/wind-forecasting/config/training/training_inputs_kestrel_awaken_pred60.yaml $HOME/toolboxes/wind_forecasting_env/wind-forecasting/config/training/training_inputs_kestrel_awaken_pred300.yaml"
 export DATA_CONFIG_PATH="$HOME/toolboxes/wind_forecasting_env/wind-forecasting/config/preprocessing/preprocessing_inputs_kestrel_awaken_new.yaml"
 
 echo "MODELS=${MODELS}"
@@ -38,5 +37,5 @@ module load mamba
 module load PrgEnv-intel
 mamba activate wind_forecasting_env
 
-python WindForecast.py --model ${MODELS} --model_config ${MODEL_CONFIG_PATH} --data_config ${DATA_CONFIG_PATH} --simulation_timestep 1 --prediction_interval 60 300 \
+python WindForecast.py --model ${MODELS} --model_config ${MODEL_CONFIG_PATH} --data_config ${DATA_CONFIG_PATH} --simulation_timestep 1 \
 	               --multiprocessor cf --max_splits 10 --prediction_type distribution --use_tuned_params --use_trained_models --rerun_validation
