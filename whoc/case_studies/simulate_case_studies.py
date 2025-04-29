04c287777ba59d0c7604bc8362aa36a03ca36ea4
Aoife Henry <aoife.henry@colorado.edu>
2025-04-29 14:08:59 -0600

    ram updates

--- whoc/case_studies/simulate_case_studies.py
+++ whoc/case_studies/simulate_case_studies.py
@@ -273,7 +273,7 @@ def simulate_controller(controller_class, wind_forecast_class, simulation_input_
     
         # if RAM is running low, write existing data to dataframe and continue
         # turn data into arrays, pandas dataframe, and export to csv
-        if ((ram_used := virtual_memory().percent) > kwargs["ram_limit"]) or (final := (t>=stoptime)):
+        if (final := (t>=stoptime)) or ((ram_used := virtual_memory().percent) > kwargs["ram_limit"]):
             logging.info(f"Used {ram_used}% RAM.")
             # turn data into arrays, pandas dataframe, and export to csv
             write_df(case_family=kwargs["case_family"],
