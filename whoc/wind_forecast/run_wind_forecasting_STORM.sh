dbb306ef23e5db82f3b51b83bb11a461d334f76c
Juan Boullosa <juan.manuel.boullosa.novo@uni-oldenburg.de>
2025-04-30 19:22:11 +0200

    ~

--- whoc/wind_forecast/run_wind_forecasting_STORM.sh
+++ whoc/wind_forecast/run_wind_forecasting_STORM.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks-per-node=1         # Requesting 1 task for 1 GPU
 #SBATCH --cpus-per-task=16          # CPUs per task (adjust based on inference needs)
 #SBATCH --mem-per-cpu=8192          # Memory per CPU (Total Mem = 1 * 16 * 8192 = 128GB)
-#SBATCH --gres=gpu:1           # Request 1 H100 GPU (Matches ntasks-per-node)
+#SBATCH --gres=gpu:H100:1           # Request 1 H100 GPU (Matches ntasks-per-node)
 #SBATCH --time=1-00:00              # Time limit (e.g., 1 hour for inference)
 #SBATCH --job-name=whoc_infer_storm
 #SBATCH --output=/user/taed7566/Forecasting/wind-forecasting/logs/slurm_logs/whoc_infer_%j.out
