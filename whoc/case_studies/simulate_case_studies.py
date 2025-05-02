619e9fbefca4b0f3796f9105c684b09372b0467e
Aoife Henry <aoife.henry@colorado.edu>
2025-05-01 23:42:02 -0600

    added lpf to FLORIS signal

--- whoc/case_studies/simulate_case_studies.py
+++ whoc/case_studies/simulate_case_studies.py
@@ -10,6 +10,7 @@ from shutil import move
 
 from whoc.interfaces.controlled_floris_interface import ControlledFlorisModel
 from whoc.wind_field.WindField import first_ord_filter
+from whoc.wind_field.WindField import butterworth_LPF_TFmag
 
 import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -161,11 +162,47 @@ def simulate_controller(controller_class, wind_forecast_class, simulation_input_
         simulation_dir = 180.0 + np.rad2deg(np.arctan2(simulation_u, simulation_v))
         simulation_dir[simulation_dir < 0] = 360. + simulation_dir[simulation_dir < 0]
         simulation_dir[simulation_dir > 360] = np.mod(simulation_dir[simulation_dir > 360], 360.) 
+        
+        # filter wind field
+        fc = 0.0011 # TODO what should this be for wind speed
+        n_lpf = 1
+        ts_len = len(simulation_dir)
+        fs = (0.5 / simulation_input_dict["simulation_dt"]) * np.array([i for i in range(1, int(ts_len / 2))]) / (ts_len / 2)
+        
+        tf_mag_lpf = butterworth_LPF_TFmag(fs, fc, n_lpf)
+
+        # FFT of raw wind direction time series
+        freq_vec_dir = np.fft.fft(simulation_dir)
+
+        # Apply LPF magnitude
+        freq_vec_dir[1:int(ts_len / 2)] *= tf_mag_lpf
+        
+        if ts_len % 2 == 0:
+            freq_vec_dir[int(ts_len / 2)] = np.sqrt(np.max([butterworth_LPF_TFmag(0.5 / simulation_input_dict["simulation_dt"], fc, n_lpf), 0]))
+            freq_vec_dir[int(ts_len / 2) + 1:] *= np.flip(tf_mag_lpf)
+        else:
+            freq_vec_dir[int(ts_len / 2):int(ts_len / 2)+2] = np.sqrt(np.max([butterworth_LPF_TFmag(0.5 / simulation_input_dict["simulation_dt"], fc, n_lpf), 0]))
+            freq_vec_dir[int(ts_len / 2) + 2:] *= np.flip(tf_mag_lpf)
+
+        # time series of low-frequency wind direction
+        simulation_dir = np.real(np.fft.ifft(freq_vec_dir))
+        
+        # START TEST
+        # import matplotlib.pyplot as plt
+        # plt.figure(figsize=(10,6))
+        # plt.plot(simulation_dir,label="Raw Wind Direction")
+        # plt.plot(new_simulation_dir,linewidth=2.0,color='r',label="Low-Frequency Wind Direction")
+        # plt.grid()
+        # plt.legend()
+        # END TEST
+        
     else:
         simulation_mag = kwargs["wind_field_ts"].select("FreestreamWindMag").to_numpy()
         simulation_dir = kwargs["wind_field_ts"].select("FreestreamWindDir").to_numpy()
         simulation_u = simulation_mag * np.sin(np.deg2rad(180 + simulation_dir))
         simulation_v = simulation_mag * np.cos(np.deg2rad(180 + simulation_dir))
+        
+        
         
     # recompute controls and step floris forward by ctrl.controller_dt
     while t < stoptime:
