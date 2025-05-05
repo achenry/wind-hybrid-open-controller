50fbf63137a4690ed05010a71994fe854a9ed737
StevenThelosen <s.f.t.m.thelosen@student.tue.nl>
2025-05-05 17:31:39 -0600

    updata ARIMA

--- whoc/case_studies/simulate_case_studies.py
+++ whoc/case_studies/simulate_case_studies.py
@@ -28,9 +28,9 @@ def simulate_controller(controller_class, wind_forecast_class, simulation_input_
     if simulation_input_dict["controller"]["uncertain"] and not wind_forecast_class.is_probabilistic:
         logging.info(f"Can't run with uncertain flag for {wind_forecast_class.__name__}, setting uncertainty off.")
         simulation_input_dict["controller"]["uncertain"] = simulation_input_dict["controller"]["uncertain"] and wind_forecast_class.is_probabilistic
-        study_name = kwargs['study_name']
+        case_name = kwargs["case_name"]
         input_df = pd.read_csv(os.path.join(results_dir, f"case_descriptions.csv"))
-        input_df.loc[input_df['study_name'] == study_name, 'uncertain'] = False
+        input_df.loc[input_df['study_name'] == case_name, 'uncertain'] = False
         #input_df.loc[int(kwargs['case_name']), "uncertain"] = False
         #input_df.to_csv(os.path.join(results_dir, f"case_descriptions.csv"))
         input_df.to_csv(os.path.join(results_dir, "case_descriptions.csv"), index=False)
@@ -40,7 +40,7 @@ def simulate_controller(controller_class, wind_forecast_class, simulation_input_
         # move(os.path.join(results_dir, f"input_config_case_{old_case_name}.pkl"), os.path.join(results_dir, f"input_config_case_{kwargs['case_name']}.pkl"))
     
     #fn = f"time_series_results_case_{kwargs['case_name']}_seed_{kwargs['wind_case_idx']}.csv"
-    fn = f"time_series_results_case_{study_name}_seed_{kwargs['wind_case_idx']}.csv"
+    fn = f"time_series_results_case_{case_name}_seed_{kwargs['wind_case_idx']}.csv"
 
     save_path = os.path.join(results_dir, fn)
     temp_save_path = os.path.join(results_dir, fn.replace(".csv", "_temp.csv"))
