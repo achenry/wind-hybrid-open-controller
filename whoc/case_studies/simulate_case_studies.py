c074cf611a4df31c92de1b208ed60d4c4dd71a45
StevenThelosen <s.f.t.m.thelosen@student.tue.nl>
2025-05-06 10:47:29 -0600

    upd

--- whoc/case_studies/simulate_case_studies.py
+++ whoc/case_studies/simulate_case_studies.py
@@ -341,8 +341,11 @@ def simulate_controller(controller_class, wind_forecast_class, simulation_input_
     
         # if RAM is running low, write existing data to dataframe and continue
         # turn data into arrays, pandas dataframe, and export to csv
+        ram_used = None
+
         if (final := (t>=stoptime)) or ((ram_used := virtual_memory().percent) > kwargs["ram_limit"]) or (len(turbine_powers_ts) >= int(3600 / simulation_input_dict["simulation_dt"])):
-            logging.info(f"Used {ram_used}% RAM.")
+            if ram_used is not None:
+                logging.info(f"Used {ram_used}% RAM.")
             # turn data into arrays, pandas dataframe, and export to csv
             write_df(case_family=kwargs["case_family"],
                     case_name=kwargs["case_name"],
