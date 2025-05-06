d77a5aa315a3c0d5f013d3de0ce746be9a7404fb
Aoife Henry <aoife.henry@colorado.edu>
2025-05-06 06:51:28 -0600

    upd

--- whoc/wind_forecast/WindForecast.py
+++ whoc/wind_forecast/WindForecast.py
@@ -1884,7 +1884,7 @@ class MLForecast(WindForecast):
         estimator_params = [param.name for param in estimator_sig.parameters.values()]
         
         # Add model-specific arguments
-        estimator_kwargs.update({k: v for k, v in checkpoint_hparams["model_config"].items() if k in estimator_params})
+        estimator_kwargs.update({k: v for k, v in checkpoint_hparams["init_args"]["model_config"].items() if k in estimator_params})
         
         # Add distr_output only if the model is NOT tactis
         if self.model_key != "tactis":
