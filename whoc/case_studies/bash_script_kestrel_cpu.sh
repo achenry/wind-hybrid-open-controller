1ec31a2a98feaba64c0b2aa6f8515af408144255
Aoife Henry <ahenry@kl5.head.cm.kestrel.hpc.nrel.gov>
2025-05-06 14:04:02 -0600

    kestrel upd

--- whoc/case_studies/bash_script_kestrel_cpu.sh
+++ whoc/case_studies/bash_script_kestrel_cpu.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 #SBATCH --job-name=cpu_floris_case_studies.py
-#SBATCH --time=24:00:00
+#SBATCH --time=96:00:00
 #SBATCH --mem=0
-#SBATCH --exclusive
-#SBATCH --nodes=1
-##SBATCH --partition=debug
+##SBATCH --exclusive
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=104
 #SBATCH --account=ssc
-##SBATCH --partition=bigmem
-##SBATCH --partition=nvme
+##SBATCH --partition=hbw
+
 # salloc --account=ssc --time=01:00:00 --partition=debug --nodes=1 --ntasks-per-node=104
 module purge
 module load mamba
 mamba activate wind_forecasting_env
-
-module load PrgEnv-intel
+module load PrgEnv-intel # NOTE: DONT NEED THIS WHEN MPI4PY IS INSTALLED WITH MAMBA, SAME GOES FOR LIBRARY LINKING LINE BELOW, ALSO DONT MARK JOB AS EXCLUSIVE
+module list
 
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/projects/ssc/ahenry/conda/envs/wind_forecasting_env/lib
 echo $SLURM_NTASKS
 
-#mpirun -np $SLURM_NTASKS python run_case_studies.py 16 17 --exclude_prediction --multiprocessor mpi -rs -rrs --ram_limit 75 --wf_source scada \
-python run_case_studies.py 19 20 --exclude_prediction --multiprocessor cf -rs -rrs --ram_limit 75 --wf_source scada \
-       -st auto -ns 10 -sd /projects/ssc/ahenry/whoc/floris_case_studies/ --generate_lut \
-       -wcnf $HOME/toolboxes/wind_forecasting_env/wind-hybrid-open-controller/examples/hercules_input_001.yaml \
-       -dcnf $HOME/toolboxes/wind_forecasting_env/wind-forecasting/config/preprocessing/preprocessing_inputs_kestrel_awaken_new.yaml \
-       -mcnf $HOME/toolboxes/wind_forecasting_env/wind-forecasting/config/training/training_inputs_kestrel_awaken_pred60_svr.yaml
+srun python run_case_studies.py 22 --exclude_prediction --multiprocessor mpi -rs -rs --ram_limit 65 --wf_source scada -st auto -ns 10 -sd /projects/ssc/ahenry/whoc/floris_case_studies/ -wcnf $HOME/toolboxes/wind_forecasting_env/wind-hybrid-open-controller/examples/hercules_input_001.yaml -dcnf $HOME/toolboxes/wind_forecasting_env/wind-forecasting/config/preprocessing/preprocessing_inputs_kestrel_awaken_new.yaml -mcnf $HOME/toolboxes/wind_forecasting_env/wind-forecasting/config/training/training_inputs_kestrel_awaken_pred60_svr.yaml
 
-
-
-
+#srun python run_case_studies.py 19 20 --exclude_prediction --multiprocessor mpi -rs -rrs --ram_limit 65 --wf_source scada -st auto -ns 10 -sd /projects/ssc/ahenry/whoc/floris_case_studies/ -wcnf $HOME/toolboxes/wind_forecasting_env/wind-hybrid-open-controller/examples/hercules_input_001.yaml -dcnf $HOME/toolboxes/wind_forecasting_env/wind-forecasting/config/preprocessing/preprocessing_inputs_kestrel_awaken_new.yaml -mcnf $HOME/toolboxes/wind_forecasting_env/wind-forecasting/config/training/training_inputs_kestrel_awaken_pred60_svr.yaml
