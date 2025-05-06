39693476727b976f405422f7d9a5d8ed7d5c5a08
Aoife Henry <aoife.henry@colorado.edu>
2025-05-06 14:30:04 -0600

    mpi testing

---
+++ whoc/test_mpi.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=cpu_floris_case_studies.py
+#SBATCH --time=00:20:00
+#SBATCH --mem=0
+##SBATCH --exclusive
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=104
+#SBATCH --account=ssc
+##SBATCH --partition=hbw
+
+# salloc --account=ssc --time=01:00:00 --partition=debug --nodes=1 --ntasks-per-node=104
+module purge
+# module load mamba
+conda activate wind_forecasting_env
+# module load PrgEnv-intel # NOTE: DONT NEED THIS WHEN MPI4PY IS INSTALLED WITH MAMBA, SAME GOES FOR LIBRARY LINKING LINE BELOW, ALSO DONT MARK JOB AS EXCLUSIVE
+module list
+
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/projects/ssc/ahenry/conda/envs/wind_forecasting_env/lib
+echo $SLURM_NTASKS
+
+srun python test_mpi.py
