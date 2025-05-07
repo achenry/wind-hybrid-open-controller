f724387c3314e3bba9bb4865253a4abc74f7f382
Aoife Henry <ahenry@kl2.head.cm.kestrel.hpc.nrel.gov>
2025-05-06 18:58:03 -0600

    upd

--- whoc/test_mpi.sh
+++ whoc/test_mpi.sh
@@ -11,8 +11,9 @@
 # salloc --account=ssc --time=01:00:00 --partition=debug --nodes=1 --ntasks-per-node=104
 module purge
 # module load mamba
-conda activate wind_forecasting_env
+conda activate test_env
 # module load PrgEnv-intel # NOTE: DONT NEED THIS WHEN MPI4PY IS INSTALLED WITH MAMBA, SAME GOES FOR LIBRARY LINKING LINE BELOW, ALSO DONT MARK JOB AS EXCLUSIVE
+module load intel
 module list
 
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/projects/ssc/ahenry/conda/envs/wind_forecasting_env/lib
