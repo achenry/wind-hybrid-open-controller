9f6c1ca663144a8232d3bf36b2669b4439f10233
StevenThelosen <s.f.t.m.thelosen@student.tue.nl>
2025-05-12 10:54:30 -0600

    ARIMA updatae

--- whoc/case_studies/initialize_case_studies.py
+++ whoc/case_studies/initialize_case_studies.py
@@ -56,7 +56,7 @@ case_studies = {
                                     "simulation_dt": {"group": 0, "vals": [60]},
                                     "floris_input_file": {"group": 0, "vals": ["../../examples/inputs/smarteole_farm.yaml"]},
                                     "uncertain": {"group": 2, "vals": [False, False]},
-                                    "wind_forecast_class": {"group": 3, "vals": ["PerfectForecast", "ARIMAForecast"]}, #, ", "KalmanFilterForecast", "SpatialFilterForecast"]},
+                                    "wind_forecast_class": {"group": 3, "vals": ["PersistenceForecast", "ARIMAForecast"]}, #, ", "KalmanFilterForecast", "SpatialFilterForecast"]},
                                     "study_name": {"group": 3, "vals": ["svr_aoifemac_flasc"]},
                                     "prediction_timedelta": {"group": 4, "vals": [420]}, #, 120, 180]},
                                     "yaw_limits": {"group": 0, "vals": ["-15,15"]},
