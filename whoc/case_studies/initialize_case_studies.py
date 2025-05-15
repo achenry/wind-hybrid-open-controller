ede824efabf4f659f28dc6c84b7ef3b06c62ba08
Aoife Henry <aoife.henry@colorado.edu>
2025-05-15 09:10:48 -0600

    upd

--- whoc/case_studies/initialize_case_studies.py
+++ whoc/case_studies/initialize_case_studies.py
@@ -95,7 +95,7 @@ case_studies = {
         "target_turbine_indices": {"group": 1, "vals": ["6,", "6,4"]},
         "uncertain": {"group": 0, "vals": [False]}, #, False]},
         "wind_forecast_class": {"group": 0, "vals": ["PerfectForecast"]},
-        "prediction_timedelta": {"group": 2, "vals": [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480, 510, 540, 570, 600, 630, 660, 720, 750, 780]}, # TODO change naming so that we need not repeat, 450, 630, 660, 690, 720]},
+        "prediction_timedelta": {"group": 2, "vals": [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480, 510, 540, 570, 600, 630, 660, 720, 750, 780, 810]},
         },
     "baseline_controllers_informer_forecasters_awaken": {
         "n_horizon": {"group": 0, "vals": [0]},
