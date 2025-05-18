6727a2147c2b6950f586aa756f8888611760146d
Juan Boullosa <juan.manuel.boullosa.novo@uni-oldenburg.de>
2025-05-19 00:29:25 +0200

    ~

--- whoc/wind_forecast/run_scripts/run_wind_forecasting_STORM.sh
+++ whoc/wind_forecast/run_scripts/run_wind_forecasting_STORM.sh
@@ -32,7 +32,7 @@ export MODEL_CONFIG_PATH_ARG=${2:-"${WF_DIR}/config/training/training_inputs_jua
 export DATA_CONFIG_PATH_ARG=${3:-"${WF_DIR}/config/preprocessing/preprocessing_inputs_flasc_STORM.yaml"}
 export CHECKPOINT_ARG=${4:-"best"} # Default to 'best' if not provided
 export MAX_STEPS_ARG=${5:-1080}    # Default to original value if not provided
-export PREDICTION_TYPE_ARG=${6:-"distribution"}
+export PREDICTION_TYPE_ARG=${6:-"sample"}
 
 # --- Create Logging Directories ---
 mkdir -p ${LOG_DIR}/slurm_logs
