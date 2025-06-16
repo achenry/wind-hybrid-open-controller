4c75400fc670a68d9cbadd691a77a3c4a91254c8
Aoife Henry <aoife.henry@colorado.edu>
2025-06-16 10:23:58 +0100

    uncertain lut fix

--- whoc/case_studies/initialize_case_studies.py
+++ whoc/case_studies/initialize_case_studies.py
@@ -59,18 +59,20 @@ case_studies = {
         "use_lut_filtered_wind_mag": {"group": 0, "vals": [True]},
         "interpolation_method": {"group": 0, "vals": ["nearest"]},
         "simulation_dt": {"group": 0, "vals": [1]},
-        "floris_input_file": {"group": 0, "vals": ["../../examples/inputs/gch_KP_v4.yaml"]},
-        "lut_path": {"group": 0, "vals": ["../../examples/inputs/gch_KP_v4_lut.csv",]},
+        "floris_input_file": {"group": 0, "vals": [
+            "../../examples/inputs/gch_KP_v4.yaml"
+                                                ]},
+        "lut_path": {"group": 0, "vals": [
+            "../../examples/inputs/gch_KP_v4_lut.csv",
+                                        ]},
         "yaw_limits": {"group": 0, "vals": ["-15,15"]},
-        "controller_class": {"group": 1, "vals": ["GreedyController", "LookupBasedWakeSteeringController", "LookupBasedWakeSteeringController"]},
+        "uncertain": {"group": 1, "vals": [True]},
+        "controller_class": {"group": 1, "vals": ["LookupBasedWakeSteeringController"]},
+        "prediction_timedelta": {"group": 1, "vals": [510]},
+        "target_turbine_indices": {"group": 1, "vals": ["74,73"]},
         "model_config_path": {"group": 1, "vals": [
-            os.path.join(os.path.dirname(wind_forecasting_file), "../config/training/training_inputs_aoifemac_awaken_predGreedy.yaml"),
-            os.path.join(os.path.dirname(wind_forecasting_file), "../config/training/training_inputs_aoifemac_awaken_predLUT.yaml"),
-            os.path.join(os.path.dirname(wind_forecasting_file), "../config/training/training_inputs_aoifemac_awaken_predLUT.yaml")]},
-        "prediction_timedelta": {"group": 1, "vals": [210, 510, 510]},
-        "uncertain": {"group": 1, "vals": [False, False, True]},
-        "target_turbine_indices": {"group": 1, "vals": ["4,", "74,73", "74,73"]},
-        "wind_forecast_class": {"group": 2, "vals": ["SVRForecast", "PerfectForecast"]}
+            os.path.join(os.path.dirname(wind_forecasting_file), "../config/training/training_inputs_kestrel_awaken_predLUT.yaml")]},
+        "wind_forecast_class": {"group": 0, "vals": ["KalmanFilterForecast"]}
     },
     "baseline_controllers_informer_forecaster_test_awaken": {
                                     "controller_dt": {"group": 0, "vals": [5]},
