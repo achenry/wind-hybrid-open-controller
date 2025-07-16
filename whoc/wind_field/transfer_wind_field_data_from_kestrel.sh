f54d1c3ec8baaef4bbd6107ecd2ae01d85d270ec
Aoife Henry <aoife.henry@colorado.edu>
2025-07-16 15:43:50 -0700

    upd

--- whoc/wind_field/transfer_wind_field_data_from_kestrel.sh
+++ whoc/wind_field/transfer_wind_field_data_from_kestrel.sh
@@ -1,10 +1,10 @@
 scp -r ahenry@kestrel.hpc.nrel.gov:/home/ahenry/toolboxes/whoc_env/wind-hybrid-open-controller/examples/post_processing /Users/ahenry/Documents/toolboxes/wind-hybrid-open-controller/examples
 
-scp -r ahenry@kestrel.hpc.nrel.gov:/projects/ssc/ahenry/whoc/floris_case_studies/ /Users/ahenry/Documents/toolboxes/wind-hybrid-open-controller/examples/
+scp -r ahenry@kestrel.hpc.nrel.gov:/projects/awaken/ahenry/whoc/floris_case_studies/ /Users/ahenry/Documents/toolboxes/wind-hybrid-open-controller/examples/
 
-scp -r ahenry@kestrel.hpc.nrel.gov:/projects/ssc/ahenry/whoc/floris_case_studies/baseline_controllers /Users/ahenry/Documents/toolboxes/wind-hybrid-open-controller/examples/floris_case_studies
-scp -r ahenry@kestrel.hpc.nrel.gov:/projects/ssc/ahenry/whoc/floris_case_studies/slsqp_solver_sweep /Users/ahenry/Documents/toolboxes/wind-hybrid-open-controller/examples/floris_case_studies
+scp -r ahenry@kestrel.hpc.nrel.gov:/projects/awaken/ahenry/whoc/floris_case_studies/baseline_controllers /Users/ahenry/Documents/toolboxes/wind-hybrid-open-controller/examples/floris_case_studies
+scp -r ahenry@kestrel.hpc.nrel.gov:/projects/awaken/ahenry/whoc/floris_case_studies/slsqp_solver_sweep /Users/ahenry/Documents/toolboxes/wind-hybrid-open-controller/examples/floris_case_studies
 
-scp -r ahenry@kestrel.hpc.nrel.gov:/projects/ssc/ahenry/whoc/floris_case_studies/wind_field_data /Users/ahenry/Documents/toolboxes/wind-hybrid-open-controller/examples/floris_case_studies/
+scp -r ahenry@kestrel.hpc.nrel.gov:/projects/awaken/ahenry/whoc/floris_case_studies/wind_field_data /Users/ahenry/Documents/toolboxes/wind-hybrid-open-controller/examples/floris_case_studies/
 
-scp -r ahenry@kestrel.hpc.nrel.gov:/projects/ssc/ahenry/whoc/floris_case_studies/wind_preview_type /Users/ahenry/Documents/toolboxes/wind-hybrid-open-controller/examples/floris_case_studies
+scp -r ahenry@kestrel.hpc.nrel.gov:/projects/awaken/ahenry/whoc/floris_case_studies/wind_preview_type /Users/ahenry/Documents/toolboxes/wind-hybrid-open-controller/examples/floris_case_studies
