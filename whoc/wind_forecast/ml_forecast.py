5ea2b21a75ecc08433b5fcad7fbd2f74ab6dfb96
Aoife Henry <aoife.henry@colorado.edu>
2025-08-16 10:56:13 -0700

    upd

--- whoc/wind_forecast/ml_forecast.py
+++ whoc/wind_forecast/ml_forecast.py
@@ -203,6 +203,11 @@ class MLForecast(WindForecast):
             if self.model_key != "tactis":
                 estimator_kwargs["distr_output"] = distr_output_class(dim=self.data_module.num_target_vars, **self.model_config["model"]["distr_output"]["kwargs"])
             
+            # Add use_pytorch_dataloader flag if specified in dataset config
+            if "use_pytorch_dataloader" in config["dataset"]:
+                estimator_kwargs["use_pytorch_dataloader"] = config["dataset"]["use_pytorch_dataloader"]
+                logging.info(f"Setting use_pytorch_dataloader={config['dataset']['use_pytorch_dataloader']} from config")
+            
             logging.info(f"Using final estimator_kwargs:\n {estimator_kwargs}")
             estimator = estimator_class(**estimator_kwargs)
             self.self_scaled = (estimator_kwargs["scaling"] == "False") or not estimator_kwargs["scaling"]
