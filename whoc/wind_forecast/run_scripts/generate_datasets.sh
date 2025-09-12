79a26dbbcf2e450c3f12b50ee0064f9fdbf1c9d4
Aoife Henry <ahenry@kl1.head.cm.kestrel.hpc.nrel.gov>
2025-09-12 16:55:36 -0600

    upd

--- whoc/wind_forecast/run_scripts/generate_datasets.sh
+++ whoc/wind_forecast/run_scripts/generate_datasets.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=104
 ##SBATCH --mem-per-cpu=10G
 #SBATCH --mem=0
 #SBATCH --partition=debug
@@ -45,5 +45,5 @@ mamba activate wind_forecasting_env
 WORKER_RANK=0
 python ../run_forecaster_validation.py --resplit_data --ram_limit 65 --model_config ${MODEL_CONFIG_PATH} --data_config ${DATA_CONFIG_PATH} --simulation_timestep 1 \
 						--save_dir /projects/awaken/ahenry/wind_forecasting/logging --multiprocessor cf --prediction_type distribution \
-						--use_tuned_params --use_trained_models --max_splits 10
+					        --use_trained_models --max_splits 30
 
