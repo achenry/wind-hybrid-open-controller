4d36f1cf482d845167906d77421fda23b316f2fc
Aoife Henry <aoife.henry@colorado.edu>
2026-02-10 12:07:36 -0800

    uip

--- whoc/wind_forecast/wind_forecast_base.py
+++ whoc/wind_forecast/wind_forecast_base.py
@@ -755,7 +755,12 @@ class WindForecast:
         """
         if study_name and optuna_storage:
             try:
-                all_study_names = [std.study_name for std in optuna_storage.get_all_studies() if re.search(f"(?<=.*_)\\d+", std_name) is not None]
+                if job_id:
+                    suffix_pattern = f"(?<=.*_)(\\d{{8}})"
+                else:
+                    suffix_pattern = f"(?<=.*_)(\\d{{14}})"
+            
+                all_study_names = [std.study_name for std in optuna_storage.get_all_studies() if re.search(suffix_pattern, std_name) is not None]
                 if study_name in all_study_names:
                     # full study name has been supplied, use it directly
                     full_study_name = study_name
