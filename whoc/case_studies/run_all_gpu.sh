3b95b9cea704a7e1525eeaec5ad530ffc5921be5
Aoife Henry <ahenry@kl6.head.cm.kestrel.hpc.nrel.gov>
2026-03-27 11:33:36 -0600

    upd

--- whoc/case_studies/run_all_gpu.sh
+++ whoc/case_studies/run_all_gpu.sh
@@ -1,4 +1,4 @@
-sbatch bash_script_kestrel_gpu.sh 15 # informer
+#sbatch bash_script_kestrel_gpu.sh 15 # informer
 sbatch bash_script_kestrel_gpu.sh 16 # autoformer
-sbatch bash_script_kestrel_gpu.sh 17 # spacetimeformer
+#sbatch bash_script_kestrel_gpu.sh 17 # spacetimeformer
 #sbatch bash_script_kestrel_gpu.sh 18 # tactis
--- whoc/wind_forecast/run_scripts/run_wind_forecasting_gpu.sh
+++ whoc/wind_forecast/run_scripts/run_wind_forecasting_gpu.sh
@@ -3,14 +3,14 @@
 #SBATCH --account=awaken
 #SBATCH --output=%j_%x.out
 #SBATCH --nodes=1
-#SBATCH --time=01:00:00
-#SBATCH --partition=debug
+##SBATCH --time=01:00:00
+##SBATCH --partition=debug
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=20G
-##SBATCH --time=72:00:00
-##SBATCH --ntasks-per-node=8
-##SBATCH --gres=gpu:4
+#SBATCH --time=72:00:00
+#SBATCH --ntasks-per-node=8
+#SBATCH --gres=gpu:4
 ##SBATCH --mem-per-cpu=60G
 # salloc --partition=debug --gres=gpu:2 --ntasks-per-node=2 --time=01:00:00 --mem-per-cpu=85G --account=awaken
 
@@ -69,4 +69,4 @@ export CUDA_VISIBLE_DEVICES=$(seq -s, 0 $(($n_devices-1)))
 #echo "Using GPUs ${CUDA_VISIBLE_DEVICES}"
 
 # taskset -c $start_core-$end_core 
-python ../run_forecaster_validation.py --run_validation --model ${MODELS} --run_name ml --model_config ${MODEL_CONFIG_PATH} --data_config ${DATA_CONFIG_PATH} --simulation_timestep 1 --save_dir /projects/awaken/ahenry/wind_forecasting/logging --checkpoint latest --multiprocessor cf --prediction_type distribution --use_trained_models --max_splits 30 --run_processing #--max_steps 1080 --rerun_validation
+python ../run_forecaster_validation.py --model ${MODELS} --run_name ml --model_config ${MODEL_CONFIG_PATH} --data_config ${DATA_CONFIG_PATH} --simulation_timestep 1 --save_dir /projects/awaken/ahenry/wind_forecasting/logging --checkpoint latest --multiprocessor cf --prediction_type distribution --use_trained_models --max_splits 30 --run_processing #--max_steps 1080 --rerun_validation --run_validation
